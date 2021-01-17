c956c8b3b30809559b08f37e5fd176a23e45f743
MarkoCvrljak <marko.cvrljak1@gmail.com>
2021-01-17 13:31:12 +0100

    adding some semantics and accessibility, working on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,8 +7,14 @@ const Home = () => {
     return (
         <>
             <Navigation />
-            <div className="home-container">
-                Home
+            <div className="home-container" role="main">
+                <article>
+                    <h1>Hello Nature</h1>
+                    <img src="./src/img/mountian.jpg" alt="nature" />
+                    <p>
+                        Some random text
+                    </p>
+                </article>
             </div>
         </>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,20 +7,22 @@ const Navigation = () => {
     const [isMobileNavOpen, setMobileNavState] = useState(false);
 
     return (
-        <div className="navigation-container">
-            <a className="logo" href="/">Logo</a>
-            <div
-                className={`mobile-menu-icon hamburger-menu ${isMobileNavOpen ? 'is-active' : ''}`}
-                onClick={() => setMobileNavState(!isMobileNavOpen)}
-            >
-                <span className="line" />
-                <span className="line" />
-                <span className="line" />
-            </div>
-            <ul className={`main-navigation ${isMobileNavOpen ? 'is-open' : ''}`}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/contact-us">Contact Us</Link></li>
-            </ul>
+        <div className="navigation-container" role="navigation">
+            <a className="logo" href="/" role="button">Logo</a>
+            <nav>
+                <div
+                    className={`mobile-menu-icon hamburger-menu ${isMobileNavOpen ? 'is-active' : ''}`}
+                    onClick={() => setMobileNavState(!isMobileNavOpen)}
+                >
+                    <span className="line" />
+                    <span className="line" />
+                    <span className="line" />
+                </div>
+                <ul className={`main-navigation ${isMobileNavOpen ? 'is-open' : ''}`}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/contact-us">Contact Us</Link></li>
+                </ul>
+            </nav>
         </div>
     );
 };
